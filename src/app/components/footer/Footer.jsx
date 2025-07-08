a0df28f955af48c6bad557801326f4eee90988ef
helberkaue <helber.avelino05@gmail.com>
2025-07-08 13:50:41 -0300

    commitando minhas mudanças

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -1,6 +1,11 @@
 const Footer = () => {
   return (
-    <div></div>
+    <div>
+      <div className="flex  justify-around items-center" >
+        <h1 className="text-3xl font-bold" >Alta gastronomia no seu dia a dia</h1>
+        <a href="">Sobre a Linha</a>
+      </div>
+    </div>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import Hero from './sections/hero/Hero';
 import Conceito_da_linha from './sections/conceito_da_linha/Conceito_da_linha';
 import Conheca_os_cortes from './sections/conheca_os_cortes/Conheca_os_cortes';
 import Onde_encontrar from './sections/onde_encontrar/Onde_encontrar';
-import Carrosel_de_receitas from './sections/carrossel_de_receitas/Carrossel_de_receitas'
+import Carrosel_de_receitas from './sections/carrossel_de_receitas/Carrossel_de_receitas';
+import Footer from './components/footer/Footer';
 
 
 
@@ -14,7 +15,7 @@ export default function Home() {
       <Conheca_os_cortes />
       <Carrosel_de_receitas />
       <Onde_encontrar />
-      
+      <Footer></Footer>
     </main>
   );
 }
