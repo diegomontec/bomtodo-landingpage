bb054db2945b99aaf5222f1cb29463e976b7e3d3
Diego Catharin <diegocmonte91@gmail.com>
2025-07-11 13:35:02 -0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,19 +3,19 @@ import "./globals.css";
 
 import { Playfair_Display, Inter, Poppins } from "next/font/google";
 
-export const titulo = Playfair_Display({
+ const titulo = Playfair_Display({
   weight: "600",
   subsets: ["latin"],
   variable: "--font-titulo"
 });
 
-export const descricao = Inter({
+ const descricao = Inter({
   weight: "300",
   subsets: ["latin"],
   variable: "--font-descricao"
 });
 
-export const button = Poppins({
+ const button = Poppins({
   weight: "200",
   subsets: ["latin"],
   variable: "--font-button"
