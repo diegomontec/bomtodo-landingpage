d5e3423ff9b963b858087269dcff263c399c27fe
Diego Catharin <diegocmonte91@gmail.com>
2025-08-04 17:34:33 -0300

    update

--- src/app/components/button/Button.jsx
+++ src/app/components/button/Button.jsx
@@ -1,9 +1,9 @@
-export default function Button({children}) {
-    return (
-        <div>
-            <button className="flex cursor-pointer border-amber-400 border-1 bg-[#ffffff] text-bg-azul font-button py-4 px-14 rounded-md shadow-md transition-colors duration-200 hover:bg-[#d3d3d3]  hover:text-bg-azul">
-                {children}
-            </button>
-        </div>
-    )
+export default function Button({ children }) {
+  return (
+    <div className="inline-block p-[2px] bg-gradient-to-r from-[#b67b2c] via-[#fff6d6] to-[#b67b2c] rounded-md shadow-md">
+      <button className="flex items-center justify-center cursor-pointer bg-white text-bg-azul font-button py-4 px-14 rounded-md transition-colors duration-200 hover:bg-[#d3d3d3] hover:text-bg-azul w-full">
+        {children}
+      </button>
+    </div>
+  );
 }
--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
-    <footer className="bg-bg-creme text-bg-azul px-6 py-6 sm:px-8 sm:py-7 md:px-16 md:py-8 lg:px-24 lg:py-16">
+    <footer className="bg-bg-creme text-bg-azul px-6 py-6 sm:px-8 sm:py-7 md:px-16 md:py-8 lg:px-24 lg:py-12">
       <div className="flex flex-col md:flex-row justify-between items-center gap-12 md:gap-24">
         <div className="flex flex-row items-center gap-8">
           <img className="w-32 h-full" src="/assets/seloQualidade.png" alt="" />
-          <h1 className="text-2xl sm:text-3xl font-titulo text-left">
+          <h1 className="text-2xl sm:text-3xl font-titulo text-left bg-clip-text text-bg-azul">
             Alta gastronomia no seu dia a dia!
           </h1>
         </div>
