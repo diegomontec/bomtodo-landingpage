8f3788486acf5d444e687134ecc3ee3dd82d4cd0
Diego Catharin <diegocmonte91@gmail.com>
2025-08-13 17:22:34 -0300

    update hero

--- src/app/sections/hero/Hero.jsx
+++ src/app/sections/hero/Hero.jsx
@@ -6,12 +6,17 @@ import Button from "@/app/components/button/Button";
 const Hero = () => {
   return (
     <div className="relative h-screen w-full" id="hero">
-      <img
-        src="/assets/receitas/Inspiratto-BT-0162.jpg"
-        alt="Fundo hero"
+      {/* <img
+        src="assets/Whisk_gif_thlyzixmmm.gif"
+        alt="Animação do Wh"
         className="absolute inset-0 w-full h-full object-cover"
       />
-
+      <div className="absolute inset-0 bg-[radial-gradient(circle,rgba(0,0,0,0.7)_0%,rgba(0,0,0,0)_70%)]"></div> */}
+      <img
+        src="assets/Image_fx (5).jpg"
+        alt="Animação do Wh"
+        className="absolute inset-0 w-full h-full object-cover"
+      />
       <div className="relative flex items-center justify-center h-full text-white text-center">
         <div className="flex gap-8 flex-col justify-center items-center">
           <div className="text-5xl mb-2">
