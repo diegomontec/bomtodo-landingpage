8705ca5bcc8553fe6a0f7262d08c2dd73e97012e
Diego Catharin <diegocmonte91@gmail.com>
2025-08-19 13:36:02 -0300

    update hero

--- src/app/sections/hero/Hero.jsx
+++ src/app/sections/hero/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
         className="absolute inset-0 w-full h-full object-cover"
       />
 
-      <div className="absolute inset-0 bg-[linear-gradient(circle,rgba(0,0,0,0.7)_0%,rgba(0,0,0,0)_70%)]"></div>
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,rgba(0,0,0,0.7)_0%,rgba(0,0,0,0)_70%)]"></div>
 
       <div className="relative flex items-center justify-center md:justify-start h-full text-white p-6 sm:p-10 md:p-28">
         <div className="flex flex-col gap-6 sm:gap-8 justify-center items-center md:items-start text-center md:text-left w-full max-w-3xl">
