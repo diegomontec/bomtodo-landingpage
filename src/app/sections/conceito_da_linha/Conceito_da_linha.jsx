64336293763e1a8ac81091091001d82435230e34
Diego Catharin <diegocmonte91@gmail.com>
2025-08-25 11:05:04 -0300

    correção botão

--- src/app/sections/conceito_da_linha/Conceito_da_linha.jsx
+++ src/app/sections/conceito_da_linha/Conceito_da_linha.jsx
@@ -2,23 +2,17 @@ const ConceitoDaLinha = () => {
   return (
     <section
       id="conceito"
-      className="relative flex flex-col items-start justify-center text-bg-creme min-h-screen border-y-4 border-[#EFBF04]"
+      className="relative border-y-4 border-[#EFBF04]"
       style={{
-        backgroundImage: "url('assets/chefeIa.jpg')",
+        backgroundImage: "url('assets/INSPIRATTO copiar_FUNDO.jpg')",
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
       }}
     >
-      {/* <div className="absolute inset-0 bg-gradient-to-r from-black via-black/50 to-transparent"></div> */}
-
-      <div className="relative z-10 w-full max-w-5xl px-6 py-32 sm:px-8 md:px-16 lg:px-24 text-left">
-        <div className="bg-bg-azul/80 md:p-12 p-10 flex  flex-col rounded-xl border-1 hover:border-bg-creme">
-          <h1 className="text-4xl sm:text-5xl font-titulo mb-8">
-            Uma linha criada para quem transforma a cozinha em palco da
-            criatividade.
-          </h1>
-
+      <div className="relative flex flex-col lg:flex-row items-center justify-between px-6 py-16 sm:px-8 md:px-16 lg:px-24 gap-12">
+        
+        <div className="bg-bg-azul/70 md:p-12 p-10 flex flex-col rounded-xl border text-bg-creme w-full lg:w-1/2">
           <div className="flex flex-col gap-8 text-2xl sm:text-3xl font-descricao leading-relaxed">
             <p>
               Inspiratto nasceu do desejo de levar até a mesa da família
@@ -31,11 +25,20 @@ const ConceitoDaLinha = () => {
               criativas e saborosas — preparadas com ingredientes que despertam
               o melhor da gastronomia dentro de casa.
             </p>
-            <p className="mt-10 inline-block text-bg-creme py-4 rounded-lg text-2xl font-descricao">
-              Linha assinada pelo chef <strong>Tarcísio Marinho</strong>
-            </p>
           </div>
         </div>
+
+        <div className="flex flex-col items-center w-full lg:w-1/2 text-center">
+          <img 
+            src="./assets/chef.png" 
+            alt="Chef Tarcísio Marinho" 
+            className="max-w-xs lg:max-w-sm mx-auto"
+          />
+          <p className="text-bg-creme px-6 py-4 bg-bg-azul/80 rounded-lg text-2xl sm:text-3xl lg:text-4xl font-descricao">
+            Linha assinada pelo chef <strong>Tarcísio Marinho</strong>
+          </p>
+        </div>
+
       </div>
     </section>
   );
