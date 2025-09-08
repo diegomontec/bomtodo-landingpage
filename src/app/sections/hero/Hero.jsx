ba50e35f6663886b35cee2dd12c7d8aea2d78e7e
Diego Catharin <diegocmonte91@gmail.com>
2025-09-08 10:34:48 -0300

    ajustes hero

--- src/app/sections/hero/Hero.jsx
+++ src/app/sections/hero/Hero.jsx
@@ -4,37 +4,41 @@ import React from "react";
 import Button from "@/app/components/button/Button";
 
 const Hero = () => {
+  const scrollToRecipes = () => {
+    document.getElementById("receitas")?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
-    <div className="relative w-full h-screen" id="hero">
+    <section id="hero" className="relative w-full h-screen">
       <img
         src="/assets/receitas/asa-airfryer-blue-cheese copy.jpg"
-        alt="Animação do Wh"
+        alt="Asa de frango preparada na airfryer com molho blue cheese"
         className="absolute inset-0 w-full h-full object-cover"
       />
 
-      <div
-        className="absolute inset-0 bg-[radial-gradient(circle_at_center,rgba(0,0,0,0.7)_0%,rgba(0,0,0,0)_50%)] 
-    md:bg-[linear-gradient(to_right,rgba(0,0,0,0.7)_0%,rgba(0,0,0,0)_50%)]"
-      ></div>
+      <div className="absolute inset-0 bg-[radial-gradient(circle_at_center,rgba(0,0,0,0.7)_0%,rgba(0,0,0,0)_50%)] md:bg-[linear-gradient(to_right,rgba(0,0,0,0.7)_0%,rgba(0,0,0,0)_50%)]" />
 
       <div className="relative flex items-center justify-center md:justify-start h-full text-white p-6 sm:p-10 md:p-28">
-        <div className="flex flex-col gap-6 sm:gap-8 justify-center items-center md:items-start text-center md:text-left w-full max-w-3xl">
-          <div className="text-4xl md:text-5xl mb-2 leading-tight">
-            <p className="font-extralight">A INSPIRAÇÃO QUE</p>
-            <p className="font-titulo">TRANSFORMA RECEITAS</p>
+        <header className="flex flex-col gap-6 md:gap-12 justify-center items-center md:items-start text-center md:text-left w-full max-w-3xl">
+          <div className="flex flex-col justify-center items-center md:items-start md:justify-start gap-8 md:gap-16 mb-2">
+            <div className="w-64">
+              <img
+                src="/assets/seloQualidade.png"
+                alt="Selo de qualidade Inspiratto"
+                className="w-full h-auto"
+              />
+            </div>
+
+            <h1 className="text-4xl md:text-5xl leading-tight">
+              <span className="block font-extralight">A INSPIRAÇÃO QUE</span>
+              <span className="block font-titulo">TRANSFORMA RECEITAS</span>
+            </h1>
           </div>
-          <Button
-            className="mx-auto md:mx-0"
-            onClick={() => {
-              const section = document.getElementById("receitas");
-              section?.scrollIntoView({ behavior: "smooth" });
-            }}
-          >
-            Você, o chef da casa!
-          </Button>
-        </div>
+
+          <Button onClick={scrollToRecipes}>Você, o chef da casa!</Button>
+        </header>
       </div>
-    </div>
+    </section>
   );
 };
 
