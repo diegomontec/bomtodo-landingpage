6748b5397f74f0e2ce5e03613b05b2daef9e5e45
helberkaue <helber.avelino05@gmail.com>
2025-09-17 15:32:58 -0300

    Continuação das alterações v2 17/09

--- src/app/components/button/Button.jsx
+++ src/app/components/button/Button.jsx
@@ -3,7 +3,7 @@ export default function Button({ children, onClick, href }) {
     <div className="p-[2px] bg-gradient-to-r from-[#b67b2c] via-[#fff6d6] to-[#b67b2c] rounded-md shadow-md">
       <button
         onClick={onClick}
-        className="flex items-center justify-center cursor-pointer bg-white text-bg-azul font-button py-4 px-24 rounded-md transition-colors duration-200 hover:bg-[#f1f1f1] hover:text-bg-azul w-full"
+        className="flex items-center justify-center cursor-pointer bg-white text-bg-azul font-button py-4 px-24 text-base lg:text-xl lg:px-32 rounded-md transition-colors duration-200 hover:bg-[#f1f1f1] hover:text-bg-azul w-full"
       >
         {children}
       </button>
