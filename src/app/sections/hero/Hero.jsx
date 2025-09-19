720d0d884d5ae3a1f6397d6c53a32a5951439ab5
helberkaue <helber.avelino05@gmail.com>
2025-09-19 16:44:08 -0300

    Atualização 19/09

--- src/app/sections/hero/Hero.jsx
+++ src/app/sections/hero/Hero.jsx
@@ -23,10 +23,10 @@ const Hero = () => {
 
       <div className="absolute inset-0 md:bg-[linear-gradient(to_right,rgba(0,0,0,0.7)_0%,rgba(0,0,0,0)_50%)]" />
 
-      <div className="relative flex items-end sm:items-center justify-center md:justify-start h-full text-white p-6 pb-12 sm:p-10 md:p-28">
+      <div className="relative flex items-center justify-center md:justify-start h-full text-white p-2 pb-12 sm:p-10 md:p-18">
         <header className="flex flex-col gap-6 md:gap-12 justify-center items-center md:items-start text-center md:text-left w-full max-w-4xl">
           <div className="flex flex-col justify-center items-center md:items-start lg:max-w-xl md:justify-start gap-8 md:gap-16 mb-2">
-            <h1 className="text-3xl md:text-5xl lg:text-6xl  leading-tight">
+            <h1 className="text-2xl md:text-5xl lg:text-6xl leading-tight">
               <span className="block font-extralight">A INSPIRAÇÃO QUE</span>
               <span className="block font-titulo">TRANSFORMA RECEITAS</span>
             </h1>
