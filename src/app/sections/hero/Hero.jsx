77c77536aab331a34ce9db9361a66174b13f756a
helberkaue <helber.avelino05@gmail.com>
2025-09-22 16:37:22 -0300

    Salvar trabalho local

--- src/app/sections/hero/Hero.jsx
+++ src/app/sections/hero/Hero.jsx
@@ -16,14 +16,19 @@ const Hero = () => {
         className="absolute inset-0 w-full h-full object-cover"
       />
       <img
+
         src="/assets/Hero mobile Inspiratto.png"
         alt="hero mobile"
         className="absolute inset-0 w-full h-full object-cover sm:hidden"
+        src="/assets/Hero mobile Inspiratto copy.png"
+        alt="hero mobile"
+        className="absolute inset-0 w-full h-screen object-cover sm:hidden"
       />
 
       <div className="absolute inset-0 md:bg-[linear-gradient(to_right,rgba(0,0,0,0.7)_0%,rgba(0,0,0,0)_50%)]" />
 
       <div className="relative flex items-center justify-center md:justify-start h-full text-white p-2 pb-12 sm:p-10 md:p-18">
+      <div className="relative flex items-center pt-64 sm:pt-0 justify-center md:justify-start h-full text-white p-2 pb-12 sm:p-10 md:p-18">
         <header className="flex flex-col gap-6 md:gap-12 justify-center items-center md:items-start text-center md:text-left w-full max-w-4xl">
           <div className="flex flex-col justify-center items-center md:items-start lg:max-w-xl md:justify-start gap-8 md:gap-16 mb-2">
             <h1 className="text-2xl md:text-5xl lg:text-6xl leading-tight">
@@ -34,6 +39,7 @@ const Hero = () => {
 
           <Button onClick={scrollToRecipes}>Você, o chef da casa!</Button>
         </header>
+      </div>
       </div>
     </section>
   );
