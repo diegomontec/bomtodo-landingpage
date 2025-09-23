38729d5d9868beb586048e6a6c6dd014a478ac43
helberkaue <helber.avelino05@gmail.com>
2025-09-23 10:15:20 -0300

    Atulizando hero mobile

--- src/app/sections/hero/Hero.jsx
+++ src/app/sections/hero/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
   };
 
   return (
-    <section id="hero" className="relative w-full h-screen">
+    <section id="hero" className="relative w-full h-full sm:h-screen">
       <img
         src="/assets/Hero Inspiratto.png"
         alt="Asa de frango preparada na airfryer com molho blue cheese"
@@ -18,12 +18,12 @@ const Hero = () => {
       <img
         src="/assets/Hero mobile Inspiratto copy.png"
         alt="hero mobile"
-        className="absolute inset-0 w-full h-screen object-cover sm:hidden"
+        className="absolute inset-0 w-full h-full object-cover sm:hidden"
       />
 
       <div className="absolute inset-0 md:bg-[linear-gradient(to_right,rgba(0,0,0,0.7)_0%,rgba(0,0,0,0)_50%)]" />
 
-      <div className="relative flex items-center justify-center md:justify-start h-full text-white p-2  sm:p-10 md:p-12">
+      <div className="relative flex items-center justify-center md:justify-start h-full text-white p-2 pb-12 sm:p-10 md:p-12">
       <div className="relative flex items-center pt-64 sm:pt-0 justify-center md:justify-start h-full text-white p-2 pb-12 sm:p-10 md:p-18">
         <header className="flex flex-col gap-6 md:gap-12 justify-center items-center md:items-start text-center md:text-left w-full max-w-4xl">
           <div className="flex flex-col justify-center items-center md:items-start lg:max-w-xl md:justify-start gap-8 md:gap-16 mb-2">
