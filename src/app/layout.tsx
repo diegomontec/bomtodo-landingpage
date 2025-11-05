1615f74aa53bd1078a3b191f08959443ca1bdd36
Diego Catharin <diegocmonte91@gmail.com>
2025-11-05 10:12:51 -0300

    tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 import { Inter, Poppins } from "next/font/google";
@@ -36,7 +37,38 @@ export default function RootLayout({
       lang="pt-BR"
       className={`${titulo.variable} ${descricao.variable} ${button.variable}`}
     >
-      <body>{children}</body>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T4DXBPNJ');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
+
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T4DXBPNJ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
+        {children}
+      </body>
     </html>
   );
 }
